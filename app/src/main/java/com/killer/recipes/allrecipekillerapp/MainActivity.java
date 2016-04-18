1850285b8d90ed7682b17f39808ac417d3d6cf89
dr-p <csdalporto@gmail.com>
2016-04-18 12:54:42 -0700

    3rd test commit

--- app/src/main/java/com/killer/recipes/allrecipekillerapp/MainActivity.java
+++ app/src/main/java/com/killer/recipes/allrecipekillerapp/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         }
         //String numberRecipes = String.valueOf((new RecipeService()).count());
         //commit change
-        //commmit change from 2nd clone of project
+        //commit change from 2nd clone of project
+        //3rd commit after updating project in primary branch
 
         TextView tv = (TextView)findViewById(R.id.textView);
         tv.setText(String.valueOf(serv.getNumRecipes()));
