7fd2dd820c2fe8895763113a793f1116df5bcc74
dr-p <csdalporto@gmail.com>
2016-04-30 17:02:40 -0700

    Yes finally i have a friend in here

--- app/src/main/java/com/killer/recipes/allrecipekillerapp/MainActivity.java
+++ app/src/main/java/com/killer/recipes/allrecipekillerapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // yes finally
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
