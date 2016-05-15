5ea588f2a6c70168c69e5be5f7c301140da87eee
dr-p <%mW_e27U%3mJ>
2016-05-15 00:30:57 -0700

    Reorganize code for ingredient search, begin reimplementation of search algorithm

--- app/src/main/java/com/killer/recipes/allrecipekillerapp/MainActivity.java
+++ app/src/main/java/com/killer/recipes/allrecipekillerapp/MainActivity.java
@@ -221,47 +221,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /* VIDUSHI */
-    public void searchByIngredientStart(View v) {
-        if (v.getId() == R.id.buttonSearchIngredients) {
-            EditText editText1 = (EditText) findViewById(R.id.editText1);
-            EditText editText2 = (EditText) findViewById(R.id.editText2);
-            EditText editText3 = (EditText) findViewById(R.id.editText3);
-            EditText editText4 = (EditText) findViewById(R.id.editText4);
 
-            String IngA = null;
-            if (editText1 != null) {
-                IngA = editText1.getText().toString();
-            }
-            String IngB = null;
-            if (editText2 != null) {
-                IngB = editText2.getText().toString();
-            }
-            String IngC = null;
-            if (editText3 != null) {
-                IngC = editText3.getText().toString();
-            }
-            String IngD = null;
-            if (editText4 != null) {
-                IngD = editText4.getText().toString();
-            }
-            Intent i = new Intent(MainActivity.this, com.killer.recipes.allrecipekillerapp.ActivityDisplayRecipe.class);
-            Bundle bun = new Bundle();
-            bun.putString("Ingredient1", IngA);
-            bun.putString("Ingredient2", IngB);
-            bun.putString("Ingredient3", IngC);
-            bun.putString("Ingredient4", IngD);
-
-            /*
-            i.putExtra("Ingredient1", IngA);
-            i.putExtra("Ingredient2", IngB);
-            i.putExtra("Ingredient3", IngC);
-            i.putExtra("Ingredient4", IngD);*/
-            startActivity(i);
-        }
-
-    }
-    /* VIDUSHI */
 
 }
 
